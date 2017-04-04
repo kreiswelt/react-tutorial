4d850a0d5a6e9bb0fca6fa500bdb01a29b1a60f3
ext-jetstar-arwin-constantine <arwin.constantine@jetstar.com>
2017-04-04 17:38:52 +0800

    re-structure

--- src/js/client.js
+++ src/js/client.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Layout from "./components/Layout";
+import Bootstrap from "./vendor/bootstrap-without-jquery";
+
+import Layout from "./pages/Layout";
 
 const app = document.getElementById('app');
 ReactDOM.render(<Layout/>, app);
--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -1,13 +1,14 @@
 import React from "react";
-import Header from "./Header";
-import Footer from "./Footer";
+
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export default class Layout extends React.Component {
 	constructor() {
 		super();
 		this.state = {name: "Kermit", title: "Welcome"};
 	}
-
+npm 
 
 	changeTitle(title) {
 		this.setState({title});
